e413aca328fa9dbbb8811a6c9579ce61ccb3fefd
Max <shadow.iqka@qq.com>
2023-04-11 13:32:22 +0800

    init project

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # yao-knowledge-web
+
+## 使用说明
+
+### 安装
+
+```bash
+npm install
+```
+
+### 调试
+
+```bash
+npm run dev
+```
+
+### 构建
+
+```bash
+npm run build
+```
---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import ErrorPage from "./error-page";
+import Index from "./routes/index";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Index />,
+    errorElement: <ErrorPage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
