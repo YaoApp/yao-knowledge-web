50c945201313c6f92c357870b1f4213dda103136
Max <shadow.iqka@qq.com>
2023-04-12 12:34:43 +0800

    [add] chat layout

--- src/routes/chat.jsx
+++ src/routes/chat.jsx
@@ -1,14 +1,12 @@
 import Header from "../components/header";
-import SearchInput from "../components/search";
+import ChatBox from "../components/chat";
 
 export default function Chat() {
   return (
     <div className="container  m-0 h-screen w-screen max-w-full	bg-hacker bg-center bg-no-repeat	bg-cover">
       <Header color={"white"} active="/chat" />
-      <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-        <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-          <SearchInput />
-        </div>
+      <div className="mx-auto max-w-screen-2xl px-20 pt-28">
+        <ChatBox />
       </div>
     </div>
   );
