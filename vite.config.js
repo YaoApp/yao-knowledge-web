bfd6d08f06e6036e2251885361b18f9796b95448
Max <shadow.iqka@qq.com>
2023-04-14 20:09:13 +0800

    [add] agent

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:5099",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
   plugins: [react()],
-})
+});
